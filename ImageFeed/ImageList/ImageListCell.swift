073060e0d9bb178b58a625325429c716fd6754e9
Owi Lover <lazygabriel@yandex.ru>
2024-08-12 05:06:45 +0300

    Created custom UITableView with images

---
+++ ImageFeed/ImageList/ImageListCell.swift
@@ -0,0 +1,20 @@
+//
+//  ImageListCell.swift
+//  ImageFeed
+//
+//  Created by Owi Lover on 8/11/24.
+//
+
+import UIKit
+
+class ImageListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImageListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var likeButton: UIButton!
+    
+}
